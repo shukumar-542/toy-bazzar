5797184db8cac35a024109b15bea43410ba9ea6c
shukumar-542 <shukumar542@gmail.com>
2023-05-19 14:54:14 +0600

    chagnging dynamic route name

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,8 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import NavBar from "../Shared/NavBar/NavBar";
 import Footer from "../Shared/Footer/Footer";
+import { useEffect } from "react";
 
 const MainLayout = () => {
+    const location = useLocation()
+    console.log(location);
+
+    useEffect(()=>{
+        if(location.pathname === '/'){
+            document.title = `ToyBazar |  Home`
+        }else{
+            document.title = `ToyBazar ${location.pathname.replace("/", " | ")}`
+        }
+    },[location])
     return (
         <div>
             <NavBar></NavBar>
